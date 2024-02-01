7467acdf04cd636d071b2532ab5c69117d516bda
wendyhamel <whamel@it4web.nl>
2024-02-01 18:45:17 +0100

    Started on html structure, added mobile menu and some styling

---
+++ src/shop.js
@@ -0,0 +1,12 @@
+window.products = function() {
+	return {
+		menuOpened: false,
+		menuItems: [
+			{title: 'Collections', url: '#'},
+			{title: 'Men', url: '#'},
+			{title: 'Women', url: '#'},
+			{title: 'About', url: '#'},
+			{title: 'Contact', url: '#'},
+		]
+	}
+}
---
+++ src/tailwind.config.js
@@ -0,0 +1,19 @@
+tailwind.config = {
+	theme: {
+		fontFamily: {
+			'sans': ['Kumbh Sans', 'sans-serif'],
+		},
+		extend: {
+			colors: {
+				'orange': 'hsl(26, 100%, 55%)',
+				'orange-pale': 'hsl(25, 100%, 94%)',
+				'blue-gray-deep': 'hsl(220, 13%, 13%)',
+				'blue-gray-dark': 'hsl(219, 9%, 45%)',
+				'blue-gray': 'hsl(220, 14%, 75%)',
+				'blue-gray-light': 'hsl(223, 64%, 98%)',
+				'white': 'hsl(0, 0%, 100%)',
+				'black': 'hsl(0, 0%, 0%)', // (with 75% opacity for lightbox background)
+			}
+		}
+	}
+}

0e40d2e768dc884ccde1e4e69147add8e59778af
wendyhamel <whamel@it4web.nl>
2024-02-06 16:24:20 +0100

    Cart js working

--- src/shop.js
+++ src/shop.js
@@ -9,12 +9,13 @@ window.products = function() {
 			{title: 'Contact', url: '#'},
 		],
 		product: {
+			id: 0,
 			company: 'Sneaker Company',
 			title: 'Fall Limited Edition Sneakers',
 			description: 'These low-profile sneakers are your perfect casual wear companion. Featuring a durable rubber outer sole, they’ll withstand everything the weather can offer.',
-			price: '125.00',
-			discount: '50',
-			fullAmount: '250.00',
+			price: 125.00,
+			discount: 50,
+			fullAmount: 250.00,
 			images: [
 				{id: 0, full: './images/image-product-1.jpg', thumb: './images/image-product-1-thumbnail.jpg', alt:'product image 1', active: true},
 				{id: 1, full: './images/image-product-2.jpg', thumb: './images/image-product-2-thumbnail.jpg', alt:'product image 2', active: false},
@@ -32,24 +33,26 @@ window.products = function() {
 		lightboxActive: false,
 		cartOpened: true,
 		count: 0,
-		order: [
-			{
-				title: '',
-				price: null,
-				imageThumb: '',
-				countInCart: 0,
-				totalPrice: null
+		order: [],
+		addToCart() {
+			this.order = this.order.filter((id) => id === this.id)
+			if (this.count === 0) {
+				this.count++
 			}
-		],
-		aadToCart() {
-			this.order.push = {
-				title: this.product.title,
-				price: null,
-				imageThumb: this.product.images[0].thumb,
-				imageAlt: this.product.images[0].alt,
-				countInCart: this.count,
-				totalPrice: this.product.price * this.order.countInCart
+			let item = {
+				id : 0,
+				title : this.product.title,
+				price : this.product.price,
+				imageThumb : this.product.images[0].thumb,
+				imageAlt : this.product.images[0].alt,
+				quantity : this.count,
+				totalPrice : this.count * this.product.price,
 			}
+			return this.order.push(item)
+		},
+		removeFromCart(id) {
+			this.count = 0
+			return this.order = this.order.filter((id) => id === this.id)
 		}
 	}
 }

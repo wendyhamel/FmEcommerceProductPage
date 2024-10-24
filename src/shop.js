e403128d63bffad6bb8f02d815169f2be0ed7a47
wendyhamel <whamel@it4web.nl>
2024-10-24 18:39:32 +0200

    Refactoring in progress

--- src/shop.js
+++ src/shop.js
@@ -8,8 +8,23 @@ window.products = function() {
 			{title: 'About', url: '#'},
 			{title: 'Contact', url: '#'},
 		],
-		product: {
-			id: 0,
+		products: [
+			{
+				company: 'Sneaker Company',
+				title: 'Fall Limited Edition Sneakers',
+				description: 'These low-profile sneakers are your perfect casual wear companion. Featuring a durable rubber outer sole, they’ll withstand everything the weather can offer.',
+				price: 125.00,
+				discount: 50,
+				fullAmount: 250.00,
+				images: [
+					{full: './images/image-product-1.jpg', thumb: './images/image-product-1-thumbnail.jpg', alt:'product image 1'},
+					{full: './images/image-product-2.jpg', thumb: './images/image-product-2-thumbnail.jpg', alt:'product image 2'},
+					{full: './images/image-product-3.jpg', thumb: './images/image-product-3-thumbnail.jpg', alt:'product image 3'},
+					{full: './images/image-product-4.jpg', thumb: './images/image-product-4-thumbnail.jpg', alt:'product image 4'},
+				]
+			},
+		],
+		selectedProduct: {
 			company: 'Sneaker Company',
 			title: 'Fall Limited Edition Sneakers',
 			description: 'These low-profile sneakers are your perfect casual wear companion. Featuring a durable rubber outer sole, they’ll withstand everything the weather can offer.',
@@ -17,77 +32,72 @@ window.products = function() {
 			discount: 50,
 			fullAmount: 250.00,
 			images: [
-				{id: 0, full: './images/image-product-1.jpg', thumb: './images/image-product-1-thumbnail.jpg', alt:'product image 1', active: true},
-				{id: 1, full: './images/image-product-2.jpg', thumb: './images/image-product-2-thumbnail.jpg', alt:'product image 2', active: false},
-				{id: 2, full: './images/image-product-3.jpg', thumb: './images/image-product-3-thumbnail.jpg', alt:'product image 3', active: false},
-				{id: 3, full: './images/image-product-4.jpg', thumb: './images/image-product-4-thumbnail.jpg', alt:'product image 4', active: false},
-			]
+				{full: './images/image-product-1.jpg', thumb: './images/image-product-1-thumbnail.jpg', alt:'product image 1'},
+				{full: './images/image-product-2.jpg', thumb: './images/image-product-2-thumbnail.jpg', alt:'product image 2'},
+				{full: './images/image-product-3.jpg', thumb: './images/image-product-3-thumbnail.jpg', alt:'product image 3'},
+				{full: './images/image-product-4.jpg', thumb: './images/image-product-4-thumbnail.jpg', alt:'product image 4'},
+			],
 		},
-		get selectedImage() {
-			return this.product.images.find((image) => image.active)
+		activeImage: {
+			full: './images/image-product-1.jpg', thumb: './images/image-product-1-thumbnail.jpg', alt:'product image 1'
 		},
-		selectImage(id) {
-			this.product.images.forEach((image) => image.active = false)
-			this.product.images.find((image) => image.id === id).active = true
+		selectImage(image) {
+			this.activeImage = image
 		},
 		get currentImageIndex() {
-			return this.product.images.findIndex((image) => image.active)
+			return this.selectedProduct.images.findIndex((image) => image.full === this.activeImage.full)
 		},
 		next: null,
 		previous: null,
-		previousImage(){
+		previousImage(selectedProduct){
 			if (this.currentImageIndex === 0) {
-				this.previous = this.product.images.length - 1
+				this.previous = selectedProduct.images.length - 1
 				this.next = this.currentImageIndex + 1
 			} else {
 				this.previous = this.currentImageIndex - 1
-				if (this.currentImageIndex === this.product.images.length - 1) {
+				if (this.currentImageIndex === selectedProduct.images.length - 1) {
 					this. next = 0
 				} else {
 					this.next = this.currentImageIndex + 1
 				}
 			}
-			this.product.images.find((image) => image.id === this.selectedImage.id).active = false
-			this.product.images.find((image) => image.id === this.product.images[this.previous].id).active = true
+			this.activeImage = this.selectedProduct.images[this.previous]
 		},
-		nextImage() {
-			if (this.currentImageIndex === this.product.images.length - 1) {
+		nextImage(product) {
+			if (this.currentImageIndex === product.images.length - 1) {
 				this.next = 0
 				this.previous = this.currentImageIndex - 1
 			} else {
 				this.next = this.currentImageIndex + 1
 				if (this.currentImageIndex === 0) {
-					this.previous = this.product.images.length - 1
+					this.previous = product.images.length - 1
 				} else {
 					this.previous = this.currentImageIndex - 1
 				}
 			}
-			this.product.images.find((image) => image.id === this.selectedImage.id).active = false
-			this.product.images.find((image) => image.id === this.product.images[this.next].id).active = true
+			this.activeImage = this.selectedProduct.images[this.next]
 		},
 		lightboxActive: false,
 		cartOpened: false,
 		count: 0,
-		order: [],
-		addToCart() {
-			this.order = this.order.filter((id) => id === this.id)
+		order: {
+			products: []
+		},
+		addToCart(product) {
 			if (this.count === 0) {
 				this.count++
 			}
-			let item = {
-				id : 0,
-				title : this.product.title,
-				price : this.product.price,
-				imageThumb : this.product.images[0].thumb,
-				imageAlt : this.product.images[0].alt,
-				quantity : this.count,
-				totalPrice : this.count * this.product.price,
-			}
-			return this.order.push(item)
-		},
-		removeFromCart(id) {
+			// imageThumb = product.images[0].thumb
+			// imageAlt = product.images[0].alt
+			// quantity = this.count
+			// totalPrice = product.quantity * product.price
 			this.count = 0
-			return this.order = this.order.filter((id) => id === this.id)
+			console.log(product)
+			this.order.products.push(product)
+		},
+		removeFromCart(product) {
+			let position = this.order.lastIndexOf(product)
+			this.order.products.splice(position, 1)
 		}
 	}
 }
